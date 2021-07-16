25845662daa9427cdb155d6446bcf2e23eef2ce7
salma <fatfieur18@gmail.com>
2021-07-16 05:16:22 +0100

    sec3

--- script.js
+++ script.js
@@ -6,19 +6,27 @@ connectDB();
 
 // Get all records older than 24 hours 
 async function fetchData() {
-    const files = await File.find({ createdAt : { $lt: new Date(Date.now() - 24 * 60 * 60 * 1000)} })
-    if(files.length) {
-        for (const file of files) {
-            try {
-                fs.unlinkSync(file.path);
-                await file.remove();
-                console.log(`successfully deleted ${file.filename}`);
-            } catch(err) {
-                console.log(`error while deleting file ${err} `);
+    try {
+        const files = await File.find({
+            createdAt: {
+                $lt: new Date(Date.now() - 24 * 60 * 60 * 1000)
+            }
+        })
+        if (files.length) {
+            for (const file of files) {
+                try {
+                    fs.unlinkSync(file.path);
+                    await file.remove();
+                    console.log(`successfully deleted ${file.filename}`);
+                } catch (err) {
+                    console.log(`error while deleting file ${err} `);
+                }
             }
         }
+        console.log('Job done!');
+    } catch (error) {
+        // your catch block code goes here
     }
-    console.log('Job done!');
 }
 
-fetchData().catch(process.exit);
+fetchData().then(process.exit);
